2db9270712c7dd151d372792932afc057ea5f07f
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-12-04 15:41:24 -0600

    WIP formatting, layout, icon

--- scripts/styleLogo.js
+++ scripts/styleLogo.js
@@ -1,10 +1,7 @@
 // Changing SVG styling requires using JS
 // https://stackoverflow.com/a/4906603
-function styleLogo() {
-  // Set the color here:
-  let fillColor = '#fff';
-
-  let svgDoc = document.getElementById('logo').contentDocument;
+function styleLogo(elid,fillColor='#fff') {
+  let svgDoc = document.getElementById(elid).contentDocument;
   let styleElement = svgDoc.createElementNS("http://www.w3.org/2000/svg", "style");
   styleElement.textContent = `svg { fill: ${fillColor}; }`;
   svgDoc.children[0].appendChild(styleElement);
